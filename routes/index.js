01e99a14e2ba323e8aa68e4daa90ee40756e37be
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2025-11-26 11:43:16 +0100

    merge of branches

--- routes/index.js
+++ routes/index.js
@@ -27,17 +27,4 @@ router.use('/products', productsRoutes);
 router.use('/orders', ordersRoutes);
 router.use('/stations', stationsRoutes);
 
-//edit pages
-router.get('/edit-user', UsersController.renderEditUser);
-
-router.get('/edit-product', ProductsController.renderEditProduct);
-
-router.get('/edit-station', StationsController.renderEditStation);
-
-router.get('/edit/order', OrdersController.renderEditOrder);
-
-//delete routes
-router.post('/delete/:id', UsersController.deleteUser);
-
-
 module.exports = router;
