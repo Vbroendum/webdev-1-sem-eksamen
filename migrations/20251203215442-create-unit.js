b110cec9299762ad96e80092f58d47a0421f5d3c
maryamissam123 <maryamissam123@gmail.com>
2025-12-03 22:55:53 +0100

    Rettelser til unit og company

--- migrations/20251203215442-create-unit.js
+++ migrations/20251203215442-create-unit.js
@@ -9,6 +9,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      unit: {
+        type: Sequelize.STRING
+      },
       short_unit: {
         type: Sequelize.STRING
       },
--- models/unit.js
+++ models/unit.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   unit.init({
+    unit: DataTypes.STRING,
     short_unit: DataTypes.STRING
   }, {
     sequelize,
