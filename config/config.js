f984991bc975804dc0ec0f24e455b5e351657614
Victor Brøndum <32537066+Vbroendum@users.noreply.github.com>
2025-12-04 12:51:05 +0100

    Rettelse af env fil og config + tilføjelse af sequelizerc fil

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports =
+{
+  "development": {
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || "root",
+    "database": process.env.DB_NAME || "webdeveksamen",
+    "host": process.env.DB_HOST || "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || "root",
+    "database": process.env.DB_NAME || "webdeveksamen",
+    "host": process.env.DB_HOST || "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || "root",
+    "database": process.env.DB_NAME || "webdeveksamen",
+    "host": process.env.DB_HOST || "localhost",
+    "dialect": "mysql"
+  }
+}
