fde03beda9ac2363602da3d2601280ffae06b4a1
joakim okkels <joakim.okkels@gmail.com>
2025-12-05 10:54:59 +0100

    seeder user, role

--- seeders/20251205092910-demo-user.js
+++ seeders/20251205092910-demo-user.js
@@ -2,24 +2,42 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        station_id: 1,
+        role_id: 1, 
+        first_name: 'Anders',
+        last_name: 'Hansen',
+        user_email: 'anders.hansen@example.com',
+        user_password: 'hashed_password_1', // skift til rigtig hash hvis du bruger bcrypt
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        station_id: 1,
+        role_id: 2,
+        first_name: 'Maria',
+        last_name: 'Larsen',
+        user_email: 'maria.larsen@example.com',
+        user_password: 'hashed_password_2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        station_id: 2,
+        role_id: 1,
+        first_name: 'Jonas',
+        last_name: 'Poulsen',
+        user_email: 'jonas.poulsen@example.com',
+        user_password: 'hashed_password_3',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
