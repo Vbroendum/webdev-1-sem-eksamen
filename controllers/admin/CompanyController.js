ce040f8d9608f65cc5b60727b8cfce11c4095a15
joakim okkels <joakim.okkels@gmail.com>
2025-12-09 14:51:16 +0100

    crud virksomhed + user-edit dansk

--- controllers/admin/CompanyController.js
+++ controllers/admin/CompanyController.js
@@ -17,3 +17,78 @@ exports.renderCompanies = async (req, res) => {
     res.status(500).send('Databasefejl');
   }
 };
+
+// RENDER - Viser formular til oprettelse af ny virksomhed
+exports.renderCreateCompanyForm = (req, res) => {
+    res.render('admin/Companies/new-company', {
+        title: 'Opret ny bruger'
+    });
+}
+
+// CREATE - Opretter en ny virksomhed
+exports.createCompany = async (req, res) => {
+    try {
+        const { name, cvr } = req.body;
+        await db.company.create({ name, cvr });
+        res.redirect('/companies');
+    } catch (error) {
+        console.error('Fejl ved oprettelse af virksomhed:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
+// RENDER - Viser formular til redigering af en virksomhed
+exports.renderEditCompany = async (req, res) => {
+    try {
+        const company = await db.company.findByPk(req.params.id, {
+            attributes: ['id', 'name', 'cvr']
+        });
+
+        if (!company) {
+            return res.status(404).send('virksomhed ikke fundet');
+        }
+
+     res.render('admin/companies/edit-company', {
+        title: 'Rediger virksomhed',
+        company
+    });
+    } catch (error) {
+        console.error('Fejl ved hentning af virksomhed:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
+
+// UPDATE - Opdaterer en eksisterende virksomhed
+exports.editCompany = async (req, res) => {
+    try {
+        const { name, cvr } = req.body;
+        await db.company.update(
+            { name, cvr },
+            { where: { id: req.params.id } }
+        );
+        res.redirect('/companies');
+    } catch (error) {
+        console.error('Fejl ved opdatering af virksomhed:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
+
+// DELETE - Sletter en virksomhed
+exports.deleteCompany = async (req, res) => {
+    try {
+       const company = await db.company.findByPk(req.params.id, {
+            attributes: ['id', 'name', 'cvr']
+        });
+
+        if (!company) {
+            return res.status(404).send('virksomhed ikke fundet');
+        }
+        
+        await db.company.destroy({
+            where: { id: req.params.id }
+        });
+        res.redirect('/companies');
+    } catch (error) {
+        console.error('Fejl ved sletning af virksomhed:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
--- routes/admin/companyRoute.js
+++ routes/admin/companyRoute.js
@@ -6,4 +6,19 @@ const CompanyController = require('../../controllers/admin/CompanyController');
 // GET - Viser alle virksomheder
 router.get('/', CompanyController.renderCompanies);
 
+// GET - viser formular til oprettelse af ny virksomhed
+router.get('/new-company', CompanyController.renderCreateCompanyForm);
+
+// // GET - Edit virksomhed
+router.get('/edit-Company/:id', CompanyController.renderEditCompany);
+
+// // POST - Delete virksomhed
+router.post('/:id/delete', CompanyController.deleteCompany);
+
+// // POST - opretter en ny virksomhed
+router.post('/', CompanyController.createCompany);
+
+// // POST - opdaterer en eksisterende virksomhed
+router.post('/:id/edit-company', CompanyController.editCompany);
+
 module.exports = router;
