832b78f5cc92291bd61420aea94634c7ac9085bc
maryamissam123 <maryamissam123@gmail.com>
2025-12-09 21:34:30 +0100

    CRUD for product

--- controllers/admin/ProductController.js
+++ controllers/admin/ProductController.js
@@ -1,11 +1,11 @@
 //ProductController.js
 const db = require('../../models');
 
-// READ - Viser alle Produkter
+// Read
 exports.renderProducts = async (req, res) => {
   try {
         const products = await db.product.findAll({
-            attributes: ['products_name']
+            attributes: ['id', 'products_name', 'unit_id']
         });
 
         res.render('admin/products/products', {
@@ -17,4 +17,70 @@ exports.renderProducts = async (req, res) => {
             console.error('Fejl ved hentning af produkter:', error);
             res.status(500).send('Databasefejl');
         }
+};
+
+// Render formular til oprettelse af nyt produkt
+exports.renderCreateProduct = (req, res) => {
+    res.render('admin/products/new-product', {
+        title: 'Opret nyt produkt'
+    });
+}
+
+// Create 
+exports.createProduct = async (req, res) => {
+    try {
+        const { products_name, unit_id } = req.body;
+        await db.product.create({ products_name, unit_id });
+        res.redirect('/products');
+    } catch (error) {
+        console.error('Fejl ved oprettelse af produkt:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
+
+// Render formular til redigering af produkt
+exports.renderEditProduct = async (req, res) => {
+    try {
+        const product = await db.product.findByPk(req.params.id, {
+            attributes: ['id', 'products_name', 'unit_id']
+        }); 
+        if (!product) {
+            return res.status(404).send('Produkt ikke fundet');
+        }
+        res.render('admin/products/edit-product', {
+            title: 'Rediger produkt',
+            product
+        });
+    } catch (error) {
+        console.error('Fejl ved hentning af produkt:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
+
+// Update
+exports.updateProduct = async (req, res) => {
+    try {
+        const { products_name, unit_id } = req.body;
+        await db.product.update(
+            { products_name, unit_id },
+            { where: { id: req.params.id } }
+        );
+        res.redirect('/products');
+    } catch (error) {
+        console.error('Fejl ved opdatering af produkt:', error);
+        res.status(500).send('Databasefejl');
+    }
+};
+
+// Delete
+exports.deleteProduct = async (req, res) => {
+    try {
+        await db.product.destroy({
+            where: { id: req.params.id }
+        });
+        res.redirect('/products');
+    } catch (error) {
+        console.error('Fejl ved sletning af produkt:', error);
+        res.status(500).send('Databasefejl');
+    }
 };
--- routes/admin/productRoute.js
+++ routes/admin/productRoute.js
@@ -3,7 +3,18 @@ const express = require('express');
 const router = express.Router();
 const ProductController = require('../../controllers/admin/ProductController');
 
-// GET - Viser alle produkter
+// Read
 router.get('/', ProductController.renderProducts);
+
+// Create
+router.get('/create', ProductController.renderCreateProduct);
+router.post('/create', ProductController.createProduct);
+
+// Update
+router.get('/edit/:id', ProductController.renderEditProduct);
+router.post('/edit/:id', ProductController.updateProduct);  
+
+// Delete
+router.post('/delete/:id', ProductController.deleteProduct);
 
 module.exports = router;
