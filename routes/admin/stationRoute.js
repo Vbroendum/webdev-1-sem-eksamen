48f6b7637d32c599c86f31fc01ecb3ee11339096
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2025-12-10 08:44:20 +0100

    crud station

--- routes/admin/stationRoute.js
+++ routes/admin/stationRoute.js
@@ -6,4 +6,19 @@ const StationController = require('../../controllers/admin/StationController');
 // GET - Viser alle stationer
 router.get('/', StationController.renderStations);
 
+// GET - viser formular til oprettelse af ny station
+router.get('/new-station', StationController.renderCreateStationForm);
+
+// GET - Edit station
+router.get('/edit-station/:id', StationController.renderEditStation);
+
+// POST - Delete station
+router.post('/:id/delete', StationController.deleteStation);
+
+// POST - opretter en ny station
+router.post('/', StationController.createStation);
+
+// POST - opdaterer en eksisterende station
+router.post('/:id/edit-station', StationController.editStation);
+
 module.exports = router;
