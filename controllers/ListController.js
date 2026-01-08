76cfd83a9fb77c43fcc7e6321dbe2252b90dcd44
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2026-01-08 14:43:45 +0100

    list routes + controller template
    For dynamic use of list component

---
+++ controllers/ListController.js
@@ -0,0 +1,11 @@
+// controllers/Listcontroller.js
+
+//Oprettes for at samle den dynamiske data 1 sted, og gøre den genbrugelig
+exports.renderList = async (req, res) => {
+    try {
+        
+
+    }catch(error) {
+
+    }
+}
--- middleware/pagination.js
+++ middleware/pagination.js
@@ -2,8 +2,8 @@
 const express = require("express");
 
 function pagination(req, res, next) {
-    const page = parseInt(req.query.page) || 10;
-    const limit = parseInt(req.query.limit) || 1;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
     const start = (page - 1) * limit;
 
     req.pagination = {page, limit, start};
